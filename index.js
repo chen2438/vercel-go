6415899474850a2e148c8f9e9be6723c1d9c46f2
Haotian Chen <admin@vayki.com>
2024-07-13 22:43:41 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/proxy', (req, res) => {
     const target = req.body.url;
